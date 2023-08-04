8a8e6aab83a7b07bd03b3c0e06d8cfd969302bd0
xindong-sony <xind.dong@sony.com>
2023-08-04 18:55:37 +0000

    prepare

---
+++ prepare.sh
@@ -0,0 +1,16 @@
+# to setup the environment for the experiments 
+pip install virtualenv
+virtualenv -p python3.9.4 kd_env 
+source kd_env/bin/activate
+
+# install the requirements
+pip install -r requirements.txt
+
+
+# download the dataset
+mkdir ../imagenet_dataset
+wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar
+wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar
+wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz
+
+./extract_ILSVRC.sh
