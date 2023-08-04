025059be040f3321eadbd21b99202634f1e5a6a5
xindong-sony <xind.dong@sony.com>
2023-08-04 19:00:06 +0000

    prepare

--- prepare.sh
+++ prepare.sh
@@ -9,6 +9,7 @@ pip install -r requirements.txt
 
 # download the dataset
 mkdir ../imagenet_dataset
+cd ../imagenet_dataset
 wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar
 wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar
 wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz
