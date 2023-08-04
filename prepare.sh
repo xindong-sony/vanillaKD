02cf396cbae2ea3b0ba48e4eeb9b987e36d59779
Ubuntu <ubuntu@ip-10-4-18-23.ap-northeast-1.compute.internal>
2023-08-04 19:35:12 +0000

    update

--- prepare.sh
+++ prepare.sh
@@ -1,17 +1,18 @@
 # to setup the environment for the experiments 
 pip install virtualenv
-virtualenv -p python3.9.4 kd_env 
+/home/ubuntu/.local/bin/virtualenv -p python3.9.4 kd_env 
+conda deactivate
 source kd_env/bin/activate
+which python
+which pip
 
 # install the requirements
+pip install torch==1.10.1+cu111 torchvision==0.11.2+cu111 torchaudio==0.10.1 -f https://download.pytorch.org/whl/cu111/torch_stable.html
 pip install -r requirements.txt
 
 
 # download the dataset
 mkdir ../imagenet_dataset
 cd ../imagenet_dataset
-wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar
-wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar
-wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz
-
-./extract_ILSVRC.sh
+cp ../vanillaKD/extract_ILSVRC.sh ./
+wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar; wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar; wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz; ./extract_ILSVRC.sh
